a64bd913e1aef8a78f0b98df3543767c8db37277
Beginneraboutlife116 <pythonforeye@gmail.com>
2021-08-24 21:21:12 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // include express and build application server
 const express = require('express')
+// include mongoose
+const mongoose = require('mongoose')
+
 const app = express()
 const port = 3000
+
+// setting mongoose to connect with mongodb
+// 'mongodb://......' is communication format. It's essential!
+// 正確路徑：mongoose.connect('mongodb://[資料庫帳號]:[資料庫密碼]@[MongoDB位置]:[port]/[資料庫名稱]')
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // create root route
 app.get('/', (req, res) => {
